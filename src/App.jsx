d761d2d549ed0bf3037660fba209e2cf47e978ae
asadbekumarov <asadbekumarov922@gmail.com>
2024-12-12 02:42:48 +0500

    end

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./components/header/Header";
 import Hero from "./components/hero/Hero";
 import "./index.css";
 
 export default function App() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <>
-      <Header />
-      <Hero />
+      <Header isOpen={isOpen} setIsOpen={setIsOpen} />
+      <Hero isOpen={isOpen} setIsOpen={setIsOpen} />
     </>
   );
 }
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Menu from "../../assets/images/header-img/menu.svg";
 import Logo from "../../assets/images/header-img/Logo.png";
 import Search from "../../assets/images/header-img/search.svg";
@@ -6,14 +6,28 @@ import Add from "../../assets/images/header-img/add.png";
 import Left from "../../assets/images/header-img/left-topbar.png";
 import Bell from "../../assets/images/header-img/bell.png";
 import Person from "../../assets/images/header-img/person.png";
-import Home from "../../assets/images/aside-img/home.svg";
-export default function Header() {
+import Google from "../../assets/images/header-img/google.svg";
+
+export default function Header({ isOpen, setIsOpen }) {
+  const [isAcc, setIsAcc] = useState(false);
+
   return (
     <>
-      <header className="pt-4 pb-6 pl-6 pr-6 bg-[#212121] flex-col">
+      <header className="pt-4 pl-6 pr-6 bg-[#212121] flex-col">
         <div className="flex items-center justify-between">
           <div className="flex items-center gap-6">
-            <img className="cursor-pointer	" src={Menu} alt="menu-img" />
+            <button
+              onClick={() => setIsOpen(!isOpen)}
+              className={`${isOpen ? "fixed top-0 left-[-300px]" : "h-scree"}`}
+            >
+              <img className="cursor-pointer" src={Menu} alt="menu-img" />
+            </button>
+            <button
+              onClick={() => setIsOpen(!isOpen)}
+              className={`${isOpen ? "h-scree" : "fixed top-0 left-[-300px]"}`}
+            >
+              <img className="cursor-pointer" src={Menu} alt="menu-img" />
+            </button>
             <span className="flex items-center gap-1">
               <img src={Logo} alt="logo-img" />
               <sup className="font-normal text-xs text-[#a6a6a6]">TR</sup>
@@ -33,15 +47,57 @@ export default function Header() {
             <img src={Add} alt="Add-img" />
             <img src={Left} alt="Left-img" />
             <img src={Bell} alt="Bell-img" />
-            <img src={Person} alt="Person" />
+            <div className="justify-end">
+              <button onClick={() => setIsAcc(!isAcc)} className="h-auto">
+                <img src={Person} alt="Person" />
+              </button>
+              <div
+                className={`fixed top-0 right-0 mt-[60px] px-5 py-5 rounded-lg bg-neutral-600 transition-transform duration-300 ${
+                  isAcc ? "translate-x-0" : "translate-x-full"
+                }`}
+              >
+                <h2 className="flex items-center  text-white gap-4">
+                  <span className="bg-blue-500 w-8 h-8 rounded-full"></span>
+                  Asadbek Umarov
+                </h2>
+                <div className="flex items-center gap-2 py-[4px]">
+                  <img src={Google} alt="" />
+                  <p className="text-white">Google hisobi</p>
+                </div>
+                <div className="flex items-center gap-2 py-[4px]">
+                  <img src={Google} alt="" />
+                  <p className="text-white">chiqish</p>
+                </div>
+                <div className="flex items-center gap-2 py-[4px]">
+                  <img src={Google} alt="" />
+                  <p className="text-white">Youtube studio</p>
+                </div>
+                <div className="flex items-center gap-2 py-[4px]">
+                  <img src={Google} alt="" />
+                  <p className="text-white">Xarid va homiyliklar</p>
+                </div>
+                <div className="flex items-center gap-2 py-[4px]">
+                  <img src={Google} alt="" />
+                  <p className="text-white">Youtube dagi ma'lumotlaringiz</p>
+                </div>
+                <div className="flex items-center gap-2 py-[4px]">
+                  <img src={Google} alt="" />
+                  <p className="text-white">qiyofa qurilma mavzusi</p>
+                </div>
+                <div className="flex items-center gap-2 py-[4px]">
+                  <img src={Google} alt="" />
+                  <p className="text-white">Til: uzbek</p>
+                </div>
+                <div className="flex items-center gap-2 py-[4px]">
+                  <img src={Google} alt="" />
+                  <p className="text-white">Xavfsiz rejim: o'chirilgan</p>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
         <div>
-          <div className="flex items-center gap-[250px] mt-6">
-            <div className="flex gap-6">
-              <img src={Home} alt="home-img" />
-              <p className="text-white">Anasayfa</p>
-            </div>
+          <div className="flex items-center gap-[250px] mt-6 justify-center">
             <div className="flex gap-3">
               <button className="text-white font-medium text-sm bg-[#4d4d4d] py-1 px-3 rounded-[30px]">
                 Kino
